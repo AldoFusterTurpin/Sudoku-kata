80e43f15b6ded101fdc73966d4bd79ec5ff32b95
Aldo <aldoft@hotmail.com>
2023-04-10 01:56:39 +0200

    Level 1: (refactor) move things from main to sudoku package

--- internal/sudoku/sudoku.go
+++ internal/sudoku/sudoku.go
@@ -1,7 +1,14 @@
 package sudoku
 
 import (
+	"fmt"
+	"io/ioutil"
+	"log"
 	"math"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
 )
 
 // MatrixIsValid checks whether the matrix of a sudoku solution is valid or not.
@@ -117,4 +124,117 @@ func gridCorrespondsToProposedSolution(grid [][]int, proposedSolution [][]int) b
 		}
 	}
 	return true
+}
+
+func SolveLevel_1(path string) error {
+	grid, err := getMatrixFromPath(path + "/grid.csv")
+	if err != nil {
+		return err
+	}
+
+	proposedSolution, err := getMatrixFromPath(path + "/solution.csv")
+	if err != nil {
+		return err
+	}
+
+	if ProposedSolutionIsValid(grid, proposedSolution) {
+		fmt.Println("The proposed solution is correct")
+		return nil
+	}
+
+	fmt.Println("The proposed solution is incorrect")
+	return nil
+}
+
+func SolveLevel_0(path string) {
+	processPathFn := WalkFn()
+	filepath.Walk(path, processPathFn)
+}
+
+// WalkFnLevel0() returns the function that will be called for each path.
+// It always returns nil as the error handling (printing) is performed inside this function.
+func WalkFn() filepath.WalkFunc {
+	return func(fullPath string, info os.FileInfo, err error) error {
+		if err != nil {
+			return nil
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		err = validateMatrixFromPath(fullPath)
+		if err != nil {
+			log.Print(err)
+		}
+		return nil
+	}
+}
+
+func validateMatrixFromPath(fullPath string) error {
+	matrix, err := getMatrixFromPath(fullPath)
+	if err != nil {
+		return err
+	}
+
+	fileName := filepath.Base(fullPath)
+	fmt.Printf("%v: ", fileName)
+
+	if MatrixIsValid(matrix) {
+		fmt.Println("The input comply with Sudoku's rules.")
+		return nil
+	}
+
+	fmt.Println("The input doesn't comply with Sudoku's rules.")
+	return nil
+}
+
+func getMatrixFromPath(fullPath string) ([][]int, error) {
+	input, err := getContentOfFile(fullPath)
+	if err != nil {
+		return nil, err
+	}
+
+	matrix, err := convertInputToMatrix(input)
+	if err != nil {
+		return nil, err
+	}
+	return matrix, nil
+}
+
+func convertInputToMatrix(input string) ([][]int, error) {
+	input = strings.ReplaceAll(input, "\n\n", "\n")
+	input = strings.ReplaceAll(input, " ", "")
+	lines := strings.Split(input, "\n")
+	var matrix [][]int
+
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+
+		line = strings.TrimSuffix(line, ",")
+		elements := strings.Split(line, ",")
+		var row []int
+
+		for _, element := range elements {
+			v, err := strconv.Atoi(element)
+			if err != nil {
+				return nil, err
+			}
+			row = append(row, v)
+		}
+
+		matrix = append(matrix, row)
+	}
+
+	return matrix, nil
+}
+
+func getContentOfFile(filePath string) (string, error) {
+	s, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return "", err
+	}
+	return string(s), nil
 }
--- main.go
+++ main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
-	"path/filepath"
-	"strconv"
-	"strings"
 
 	"github.com/AldoFusterTurpin/Sudoku-kata/internal/sudoku"
+)
+
+const (
+	pathDataLevel0 = "/data/level0"
+	pathDataLevel1 = "/data/level1"
 )
 
 func main() {
@@ -24,124 +25,12 @@ func main() {
 
 	switch *level {
 	case 0:
-		solveLevel_0(cwd + "/data/level0")
-	default:
-		if err := solveLevel1(cwd + "/data/level1"); err != nil {
+		sudoku.SolveLevel_0(cwd + pathDataLevel0)
+	case 1:
+		if err := sudoku.SolveLevel_1(cwd + pathDataLevel1); err != nil {
 			fmt.Println(err)
 		}
+	default:
+		fmt.Printf("Unknown Level %v. Valid values: 0 or 1", *level)
 	}
-}
-
-func solveLevel1(path string) error {
-	grid, err := getMatrixFromPath(path + "/grid.csv")
-	if err != nil {
-		return err
-	}
-	fmt.Println(grid)
-
-	proposedSolution, err := getMatrixFromPath(path + "/solution.csv")
-	if err != nil {
-		return err
-	}
-
-	if sudoku.ProposedSolutionIsValid(grid, proposedSolution) {
-		fmt.Println("The proposed solution is correct")
-		return nil
-	}
-
-	fmt.Println("The proposed solution is incorrect")
-	return nil
-}
-
-func solveLevel_0(path string) {
-	processPathFn := WalkFn()
-	filepath.Walk(path, processPathFn)
-}
-
-// WalkFnLevel0() returns the function that will be called for each path.
-// It always returns nil as the error handling (printing) is performed inside this function.
-func WalkFn() filepath.WalkFunc {
-	return func(fullPath string, info os.FileInfo, err error) error {
-		if err != nil {
-			return nil
-		}
-
-		if info.IsDir() {
-			return nil
-		}
-
-		err = validateMatrixFromPath(fullPath)
-		if err != nil {
-			log.Print(err)
-		}
-		return nil
-	}
-}
-
-func validateMatrixFromPath(fullPath string) error {
-	matrix, err := getMatrixFromPath(fullPath)
-	if err != nil {
-		return err
-	}
-
-	fileName := filepath.Base(fullPath)
-	fmt.Printf("%v: ", fileName)
-
-	if sudoku.MatrixIsValid(matrix) {
-		fmt.Println("The input comply with Sudoku's rules.")
-		return nil
-	}
-
-	fmt.Println("The input doesn't comply with Sudoku's rules.")
-	return nil
-}
-
-func getMatrixFromPath(fullPath string) ([][]int, error) {
-	input, err := getContentOfFile(fullPath)
-	if err != nil {
-		return nil, err
-	}
-
-	matrix, err := convertInputToMatrix(input)
-	if err != nil {
-		return nil, err
-	}
-	return matrix, nil
-}
-
-func convertInputToMatrix(input string) ([][]int, error) {
-	input = strings.ReplaceAll(input, "\n\n", "\n")
-	input = strings.ReplaceAll(input, " ", "")
-	lines := strings.Split(input, "\n")
-	var matrix [][]int
-
-	for _, line := range lines {
-		if line == "" {
-			continue
-		}
-
-		line = strings.TrimSuffix(line, ",")
-		elements := strings.Split(line, ",")
-		var row []int
-
-		for _, element := range elements {
-			v, err := strconv.Atoi(element)
-			if err != nil {
-				return nil, err
-			}
-			row = append(row, v)
-		}
-
-		matrix = append(matrix, row)
-	}
-
-	return matrix, nil
-}
-
-func getContentOfFile(filePath string) (string, error) {
-	s, err := ioutil.ReadFile(filePath)
-	if err != nil {
-		return "", err
-	}
-	return string(s), nil
 }
